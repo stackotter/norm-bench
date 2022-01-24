c88b84aace67af2aa4076d3521e31fc1f9f26364
stackotter <stackotter@stackotter.dev>
2022-01-25 08:56:18 +1000

    Don't show the n letter word when shuffling the letters

--- src/frontend/src/lib/room.ts
+++ src/frontend/src/lib/room.ts
@@ -29,23 +29,29 @@ export type Room = {
 
 export const shuffleLetters = () => {
     room_store.update(room => {
-        let currentIndex = room.letters.length, randomIndex;
+        while (true) {
+            let currentIndex = room.letters.length, randomIndex;
 
-        while (currentIndex != 0) {
-            randomIndex = Math.floor(Math.random() * currentIndex);
-            currentIndex--;
+            while (currentIndex != 0) {
+                randomIndex = Math.floor(Math.random() * currentIndex);
+                currentIndex--;
 
-            [room.letters[currentIndex], room.letters[randomIndex]] = [
-            room.letters[randomIndex], room.letters[currentIndex]];
-        }
+                [room.letters[currentIndex], room.letters[randomIndex]] = [
+                room.letters[randomIndex], room.letters[currentIndex]];
+            }
 
-        // Make sure that we don't show them the 7 letter word.
-        // TODO: optimise, at the moment it just checks every word (whereas it only needs to check the 7 letter words).
-        let shownWord = room.letters.join();
-        for (const word of room.words) {
-            if (shownWord == word.word) {
-                shuffleLetters()
-                return
+            // Make sure that we don't show them the 7 letter word.
+            let isWord = false;
+            let shownWord = room.letters.join();
+            for (const word of room.words) {
+                if (shownWord == word.word) {
+                    isWord = true;
+                    break
+                }
+            }
+
+            if (!isWord) {
+                break
             }
         }
 
