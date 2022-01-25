64dcda44bc71d5039b1f02f13b713cac10a44737
stackotter <stackotter@stackotter.dev>
2022-01-25 18:38:38 +1000

    Update link colour to NMSS orange

--- src/frontend/src/routes/create.svelte
+++ src/frontend/src/routes/create.svelte
@@ -93,7 +93,7 @@
     }
 
     .link {
-        color: #0099ff;
+        color: #EF894A;
         text-align: center;
         margin-top: 1.5rem !important;
         margin-bottom: 1.5rem !important;
